1d4855e32bc9360a247c1a7f5d5352d4d578a909
Gregory Melnikov <grigoriy.melnikov@phystech.edu>
2025-06-06 16:43:11 +0200

    feat: minor fixes to components and configs to prepare build for IPFS
    Signed-off-by: Gregory Melnikov <grigoriy.melnikov@phystech.edu>

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out',
+  assetPrefix: './',
   experimental: {
     serverComponentsExternalPackages: ["sequelize", "pino", "pino-pretty"],
   },
   images: {
     domains: ["static.gearbox.fi"],
+    unoptimized: true,
   },
   webpack: (config) => {
     config.module.rules.push({
